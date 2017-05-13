20591e5de4eabec6d37b558f1669d90c86910d43
paradisefall123 <league@WorthyMini.local>
2017-05-13 11:26:09 -0700

    zombies
    I did knock knock jokes.

---
+++ src/MorningZombie.java
@@ -0,0 +1,19 @@
+import javax.swing.JOptionPane;
+
+public class MorningZombie {             
+	public static void main(String[] args) { 
+		System.out.println("I like dragons.");
+		JOptionPane.showMessageDialog(null,"Hey, Angie I'll give you instructions to be normal");
+		JOptionPane.showMessageDialog(null," First,brush your teeth");
+		JOptionPane.showMessageDialog(null,"Second,change into clothes");
+		JOptionPane.showMessageDialog(null,"Eat breakfast");
+		JOptionPane.showMessageDialog(null,"Brush your teeth again");
+		JOptionPane.showMessageDialog(null,"Brush your hair");  
+		JOptionPane.showMessageDialog(null,"Go to school");
+		JOptionPane.showMessageDialog(null,"Also don't eat anyones brains!");
+	}
+	
+	
+	
+	
+}
---
+++ src/knockknockjokes.java
@@ -0,0 +1,12 @@
+import javax.swing.JOptionPane;
+
+public class knockknockjokes {
+     public static void main(String[] args) {
+		JOptionPane.showInputDialog("Knock Knock");
+		JOptionPane.showInputDialog("Tank you.");
+		JOptionPane.showInputDialog("Your welcome.");
+		JOptionPane.showInputDialog("Knock Knock");
+		JOptionPane.showInputDialog("Spell");
+		JOptionPane.showInputDialog("W-H-O");
+	}
+}
