6aa6e2394a68a901dfb5af69f54f3c21342cd69a
CeilaScarlet <league@Admins-Mac-mini.local>
2017-05-27 11:25:58 -0700

    Riddles
    I wrote dragon riddles.

---
+++ src/Riddler.java
@@ -0,0 +1,14 @@
+import javax.swing.JOptionPane;
+
+public class Riddler {
+    public static void main(String[] args) {
+		JOptionPane.showInputDialog("My first is a creature whose breeding is unclear. My second, a price you must pay. My whole can be found in the river of Time and refers to events of today. What am I?");
+		JOptionPane.showMessageDialog(null,"I am a current!");
+		JOptionPane.showInputDialog("A boy Was Born In 1955 he just had his 18th birth day today how did that happen");
+		JOptionPane.showMessageDialog(null,"1955 is not the year he was born it was the hospital room he was born in");
+		JOptionPane.showInputDialog("Some say we are red, some say we are green. Some play us, some spray us what are we");
+		JOptionPane.showMessageDialog(null,"I am pepper");
+		JOptionPane.showInputDialog("Ruled by the heavens and Neptune's will,Always changing, never still, raging,roaring, placid or calm, my conflicting emotions can soothe or alarm what am I?");
+		JOptionPane.showMessageDialog(null,"I am the tides");
+	}
+}
---
+++ src/warmup.java
@@ -0,0 +1,12 @@
+import javax.swing.JOptionPane;
+
+public class warmup {
+	public static void main(String[] args) {
+		JOptionPane.showMessageDialog(null, "I like dragons");
+		JOptionPane.showMessageDialog(null, "I also like unicorns");
+		JOptionPane.showMessageDialog(null, "Another thing is I like to watch Fairy tail");
+		JOptionPane.showMessageDialog(null, "Also I like tacos");
+		JOptionPane.showMessageDialog(null, "I like the color white and purple");
+		
+	}
+}
