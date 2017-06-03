67ffce9cf3e52607849c0db3673dc8f0c9905bd4
DemoliciousDt <league@Thes-Mac-mini.local>
2017-06-03 11:28:05 -0700

    You
    You drew a box with a triangle

---
+++ src/Graffiti.java
@@ -0,0 +1,49 @@
+import java.awt.Color;
+
+import org.jointheleague.graphical.robot.Robot;
+
+public class Graffiti {
+	public static void main(String[] args) {
+		Robot yui = new Robot();
+		yui.penDown();
+		yui.setPenColor(Color.WHITE);
+		yui.setSpeed(10);
+		yui.move(250);
+		yui.turn(90);
+		yui.move(250);
+		yui.turn(90);
+	    yui.move(250);
+		yui.turn(90);
+		yui.move(250);
+		yui.move(250);
+		yui.turn(90);
+		yui.move(250);
+		yui.turn(90);
+		yui.move(250);
+		yui.turn(90);
+		yui.move(250);
+		yui.move(250);
+		yui.turn(90);
+		yui.move(250);
+		yui.turn(90);
+		yui.move(250);
+		yui.turn(90);
+		yui.move(250);
+		yui.move(250);
+		yui.turn(90);
+		yui.move(250);
+		yui.turn(90);
+		yui.move(250);
+		yui.move(250);
+		yui.turn(120);
+		yui.move(300);
+		yui.move(250);
+		yui.turn(120);
+		yui.move(250);
+		yui.move(150);
+		
+		
+
+	}
+
+}
