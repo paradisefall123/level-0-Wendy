e9ec967cc5fab96844bfaf54a7687c7a8965a428
DemoliciousDt <league@Thes-Mac-mini.local>
2017-11-25 11:33:09 -0800

    ok
    ok

--- src/VampireDetector.java
+++ src/VampireDetector.java
@@ -4,17 +4,18 @@ public class VampireDetector {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		boolean vamps;
+		String  vampire=JOptionPane.showInputDialog("Hello,I would like to get to know you, so here is a simple question, do you love going to the beach in a sunny day?");
+		
+		String   garlic=JOptionPane.showInputDialog("Thanks, ok next question, Do you love garlic because I do!");
+		
+		String  age=JOptionPane.showInputDialog("This must be a little personal but how old are you?");
+		int number= Integer.parseInt(age);
+	if (vampire.equals("no")&&  (garlic.equals("no")) && (number>100)){
+		vamps = true;
+	}else	
 	
-		String  vamps=JOptionPane.showInputDialog("Hello,I would like to get to know you, so here is a simple question, do you love going to the beach in a sunny day?");
-		if (vamps.equals("yes")){
-			JOptionPane.showMessageDialog(null,"That's great me too!");
-		}else{
-			JOptionPane.showMessageDialog(null, "OK");
-		}
-		String   blood=JOptionPane.showInputDialog("Thanks, ok next question, Do you love garlic because I do!");
-		String  garlic=JOptionPane.showInputDialog("This must be a little personal but how old are you?");
-		
-		
+	}
 		
 		
 	}
