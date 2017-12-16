0126f42c672974461558092b655a4d69a913a361
tactreconx <league@Admins-Mac-mini.local>
2017-12-16 09:46:25 -0800

    Im finished
    Im finished doing story.

--- src/AssassinsFate.java
+++ src/AssassinsFate.java
@@ -2,6 +2,9 @@ import javax.swing.JOptionPane;
 
 public class AssassinsFate {
 public static void main(String[] args) {
-String chicken=JOptionPane.showInputDialog("You are with your two siblings, coming back from a mission,but you had another one coming back home in time for the chicken.(Should you run or walk)");
+String chicken=JOptionPane.showInputDialog("You are with your two siblings, coming back from a mission, but you had another mission, coming back home in time for the chicken dinner.Shouls you run or walk?");
+
+
+
 }
 }
