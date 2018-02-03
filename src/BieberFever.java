35a642bdb3fb694593ed07fe7485b1c5a9e20b69
paradisefall123 <league@WTS-MM-7.local>
2018-02-03 11:26:43 -0800

    I finished where is waldo and i need to finish bieber fever

---
+++ src/BieberFever.java
@@ -0,0 +1,37 @@
+
+public class BieberFever {
+
+	public static void main(String[] args) {
+	for(int i= 1; i<6; i++) {
+		if (i == 4) {
+		System.out.println("no");
+		
+		
+	}else {
+		System.out.println("baby");
+		
+		
+		
+	}
+	}
+		
+	
+		// TODO Auto-generated method stub
+			for(int i = 1; i<6; i++ ) {
+				if(i==4) {
+				System.out.println("oh");
+				}else if(i==5){
+				System.out.println("I thought you'd always be mine");	
+					
+					
+					
+				}else {
+					System.out.println("baby");
+				}
+				}
+			
+			}
+			}
+	
+
+
