19cbb278d3f7a2f6457d2fc9e24f4297d49e0645
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-02-10 11:28:26 -0800

    I finished Bieber Fever and i need to finish hide the creeper.

--- src/BieberFever.java
+++ src/BieberFever.java
@@ -2,13 +2,14 @@
 public class BieberFever {
 
 	public static void main(String[] args) {
+		String word = "baby";
 	for(int i= 1; i<6; i++) {
 		if (i == 4) {
-		System.out.println("no");
+		System.out.println("oh");
 		
 		
 	}else {
-		System.out.println("baby");
+		System.out.println(word);
 		
 		
 		
@@ -17,16 +18,16 @@ public class BieberFever {
 		
 	
 		// TODO Auto-generated method stub
-			for(int i = 1; i<6; i++ ) {
+			for(int i = 2; i<6; i++ ) {
 				if(i==4) {
-				System.out.println("oh");
+				System.out.println("no");
 				}else if(i==5){
 				System.out.println("I thought you'd always be mine");	
 					
 					
 					
 				}else {
-					System.out.println("baby");
+					System.out.println(word);
 				}
 				}
 			
