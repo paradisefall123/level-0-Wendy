59f466babbb58a74f65e387d48e3c798f399b8a6
DemoliciousDt <league@Thes-Mac-mini.local>
2018-02-24 11:25:28 -0800

    I finished simple sorter and i need to finish pong
    Mr. Dino owes me animal crackers.

---
+++ pong/pong.pde
@@ -0,0 +1,16 @@
+int xPos=2;
+void setup(){
+ 
+  size(1000,1000);
+  
+  
+  
+  
+}
+void draw(){
+  background(198,195,245);
+  ellipse(500,500,100,100);
+  fill(133,189,193);
+  stroke(7,147,232);
+  
+}
--- src/SimpleSorter.java
+++ src/SimpleSorter.java
@@ -4,12 +4,40 @@ public class SimpleSorter {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		String num1=JOptionPane.showInputDialog("PLease give me one number!");
-		int number =Integer.parseInt(num1);
-		String num2=JOptionPane.showInputDialog("Another number please!");
-		int number2= Integer.parseInt(num2);
-		String num3=JOptionPane.showInputDialog("One more number!!");
-		int number3=Integer.parseInt(num3);
-		
-}
+		int biggest;
+		int middle;
+		int littlest;
+		String num1 = JOptionPane.showInputDialog("Please give me one number!");
+		int number = Integer.parseInt(num1);
+		String num2 = JOptionPane.showInputDialog("Another number please!");
+		int number2 = Integer.parseInt(num2);
+		String num3 = JOptionPane.showInputDialog("One more number!!");
+		int number3 = Integer.parseInt(num3);
+		if (number > number2) {
+			biggest = number;
+			middle = number2;
+			littlest = number3;
+		} else {
+			biggest = number2;
+			middle = number;
+			littlest = number3;
+		}
+		if (number3 > biggest) {
+			littlest = middle;
+			middle = biggest;
+			biggest = number3;
+		} else if (number3 > middle) {
+			littlest = middle;
+			middle = number3;
+
+		}
+		System.out.println("The ascending order is...");
+		System.out.println(littlest);
+		System.out.println(middle);
+		System.out.println(biggest);
+		System.out.println("The descending order...");
+		System.out.println(biggest);
+		System.out.println(middle);
+		System.out.println(littlest);
+	}
 }
