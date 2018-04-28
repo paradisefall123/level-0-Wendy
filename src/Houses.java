72cded18704418d97b78de85eb2657bcfa4706ee
Nancy Guerrero <nancyguerrero0104@gmail.com>
2018-04-28 11:25:49 -0700

    I finished houses and need to finish forloop gaulets

--- src/Houses.java
+++ src/Houses.java
@@ -1,9 +1,11 @@
 import java.awt.Color;
+import java.util.Random;
 
 import org.jointheleague.graphical.robot.Robot;
 
 public class Houses {
 public static void main(String[] args) {
+	
   Robot paola = new Robot();
   paola.setSpeed(20);
   paola.turn(-90);
@@ -12,17 +14,39 @@ public static void main(String[] args) {
   paola.move(400);
   paola.turn(-180);
   paola.penDown();
-  paola.setPenColor(Color.MAGENTA);
-  paola.move(100);
-  paola.turn(90);
-  paola.move(80);
-  paola.turn(-270);
-  paola.move(100);
-  paola.turn(-90);
-  paola.setPenColor(Color.GREEN);
-  paola.move(40);
-  for(int i=1;i<10;i++) {
-	paola.turn(-90);
+paola.setWindowColor(Color.black);
+  for(int i=1;i<21;i++) {
+	  Random number = new Random();
+		int height = number.nextInt(400);
+	
+			int red= number.nextInt(255);
+			int green = number.nextInt(50);
+			int blue = number.nextInt(255);
+		  paola.setPenColor(red, green, blue);
+		  if(height>=300) {
+		  paola.move(height+100);
+		  paola.turn(90);
+		  paola.move(80);
+		  paola.turn(-270);
+		  paola.move(height+100);
+		  paola.turn(-90);
+		  paola.setPenColor(Color.GREEN);
+		  paola.move(40);
+		  paola.turn(-90);	
+		  }else {
+			  paola.move(height+100);
+			  paola.turn(40);
+			  paola.move(30);
+			  paola.turn(90);
+			  paola.move(30);
+			  paola.turn(50);
+			  paola.move(height+100);
+			  paola.turn(-90);
+			  paola.setPenColor(Color.GREEN);
+			  paola.move(40);
+			  paola.turn(-90);	
+		  }
+		
   }
 }
 }
