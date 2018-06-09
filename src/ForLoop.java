734d4bdab7bfeb9fd1e69eb201a5067ac47534c3
paradisefall123 <league@WorthyMini-3.local>
2018-06-09 11:27:04 -0700

    finished the quizzes i am now doing the practice test

--- src/ForLoop.java
+++ src/ForLoop.java
@@ -4,7 +4,7 @@ public class ForLoop {
 	for (int i = 1; i < 11; i++) {
 			System.out.println(i);
 		}
-		for (int i = 75; i >= 50; i--) {
+		for (int i = 75; i > 50; i--) {
 			System.out.println(i);
 		}
 	}
